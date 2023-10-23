41791d7ec9765dff1e2e4d626d45636489a0a0e7
Ankit Vatsa <103623899+ankit-vatsa@users.noreply.github.com>
2023-10-23 23:55:23 +0530

    Update 001. Kotlin_Intro.kt

--- 001. Kotlin_Intro.kt
+++ 001. Kotlin_Intro.kt
@@ -16,3 +16,9 @@ When () {
   condition1 -> body1
   condition2 -> body2
 }
+
+Nullable Variables Definition
+
+Just to make any data type to a nullable type just add ‘?’ after its type.
+E.g.: var ankit: int? = 24
+
